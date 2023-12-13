fbe3cc02ac57cf073601a3a7a1318d615aae5ac2
Alex <alexgjohn@outlook.com>
2023-12-13 18:05:29 +0000

    started building hangman drawing with styleing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
 import React, { useState } from 'react';
+import wordList from './wordList.json';
+import { HangmanDrawing } from './components/HangmanDrawing';
+import { HangmanWord } from './components/HangmanWord';
+import { Keyboard } from './components/Keyboard';
 
 
 function App() {
 
-  const [wordToGuess, setWordToGuess] = useState("test")
+  const [wordToGuess, setWordToGuess] = useState<string>(() => {
+    return wordList[Math.floor(Math.random() * wordList.length)]
+  })
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
+
   return (
-    <h1>Hi</h1>
+    <div style={{
+      maxWidth: "800px",
+      display: "flex",
+      flexDirection: "column",
+      gap: "2rem",
+      margin: "0 auto",
+      alignItems: "center"
+    }}>
+      <div style={{ 
+        fontSize: "2rem",
+        textAlign: "center"
+      }}>
+        Lose
+        Win
+      </div>
+      <HangmanDrawing/>
+      {/* <HangmanWord/>
+      <Keyboard/> */}
+    </div>
   );
 }
 
